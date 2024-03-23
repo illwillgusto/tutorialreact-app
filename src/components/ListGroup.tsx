87a24738d25cdcb4ce6d8d0053fff2cc84b7fd85
Will Walton <will@endless3dge.com>
2024-03-23 11:26:08 -0500

    Update ListGroup.tsx
    reverted code back to original state

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,14 +2,11 @@ function ListGroup() {
 	let items = ['Houston', 'New York', 'Chicago', 'Atlanta', 'Cleveland'];
 	items = [];
 
-	const getMessage = () => {
-		return items.length === 0 ? <p>No item found</p> : null;
-	};
 
 	return (
 		<>
 			<h1>List</h1>
-			{getMessage()}
+			{items.length === 0 ? <p>No item found</p> : null}
 			<ul className='list-group'>
 				{items.map((item) => (
 					<li key={item}>{item}</li>
