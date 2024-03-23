46f40b63c4b12f27ef26f998bf1e9b1b385c013a
Will Walton <will@endless3dge.com>
2024-03-23 11:39:23 -0500

    Update ListGroup.tsx
    added style from bootstrap back
    also added onClick event to show the message "Clicked" in the console

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,6 +1,6 @@
 function ListGroup() {
 	let items = ['Houston', 'New York', 'Chicago', 'Atlanta', 'Cleveland'];
-	items = [];
+	
 
 
 	return (
@@ -9,7 +9,7 @@ function ListGroup() {
             {items.length === 0 && <p>No item found</p>}
 			<ul className='list-group'>
 				{items.map((item) => (
-					<li key={item}>{item}</li>
+					<li className="list-group-item" key={item} onClick={() => console.log("Clicked")}>{item}</li>
 				))}
 			</ul>
 		</>
