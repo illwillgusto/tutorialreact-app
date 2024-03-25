7a133cbbd7f95e9e081cb286908ac776776dcd7e
Will Walton <will@endless3dge.com>
2024-03-25 16:29:52 -0500

    notify selected item
    added the onSelectItem to the interface function
    
    this will notify the App.tsx on which item was selected from the component that will be relayed in the console

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import ListGroup from './components/ListGroup';
 
 function App() {
-	let items = ['Houston', 'New York', 'Chicago', 'Atlanta', 'Cleveland']; // this is moved from the ListGroups component because we added the interface function 
+	let items = ['Houston', 'New York', 'Chicago', 'Atlanta', 'Cleveland']; // this is moved from the ListGroups component because we added the interface function
 
+	const handleSelectItem = (item: string) => {
+		console.log(item);
+	};
 	return (
 		<div>
-			<ListGroup items={items} heading='Cities' /> {/*this is in response to the interface function added so the item is 'items' and the heading is a string which is 'Cities' */}
+			<ListGroup
+				items={items}
+				heading='Cities'
+				onSelectItem={handleSelectItem}
+			/>{' '}
+			{/*this is in response to the interface function added so the item is 'items' and the heading is a string which is 'Cities' */}
 		</div>
 	); // worth noting each component has it's own state
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,24 +1,20 @@
-import { useState } from "react";
-// { items: [], heading: string}, to do this we will use interface 
+import { useState } from 'react';
+// { items: [], heading: string}, to do this we will use interface
 interface ListGroupProps {
-    items: string[];
-    heading: string;
+	items: string[];
+	heading: string;
+	// (item: string) => void
+	onSelectItem: (item: string) => void;
 }
 
+function ListGroup({ items, heading, onSelectItem }: ListGroupProps) {
+	//this is a hook, a hook is a function that allows us to tap into built-in features in react
+	const [selectedIndex, setSelectedIndex] = useState(-1);
+	//these are 2 elements
 
-function ListGroup({items, heading}: ListGroupProps) {
-    
-    //this is a hook, a hook is a function that allows us to tap into built-in features in react 
-    const [selectedIndex, setSelectedIndex] = useState(-1);
-    //these are 2 elements 
-    
-    
-    
-    // arr[0] // variable (selectedIndex)
-    // arr[1] // updater function, we can update the variable above and react will know to change the state of our component and re-render our component and the DOM will be updated 
+	// arr[0] // variable (selectedIndex)
+	// arr[1] // updater function, we can update the variable above and react will know to change the state of our component and re-render our component and the DOM will be updated
 
-
-    
 	return (
 		<>
 			<h1>{heading}</h1>
@@ -26,9 +22,16 @@ function ListGroup({items, heading}: ListGroupProps) {
 			<ul className='list-group'>
 				{items.map((item, index) => (
 					<li
-						className={ selectedIndex === index ? 'list-group-item active' : 'list-group-item' }
+						className={
+							selectedIndex === index
+								? 'list-group-item active'
+								: 'list-group-item'
+						}
 						key={item}
-						onClick={() => { setSelectedIndex(index); }}>
+						onClick={() => {
+							setSelectedIndex(index);
+							onSelectItem(item);
+						}}>
 						{item}
 					</li>
 				))}
