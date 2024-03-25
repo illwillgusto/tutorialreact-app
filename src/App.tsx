2a669e125bab22a8934afc87e122d0b6e84089d1
Will Walton <will@endless3dge.com>
2024-03-25 17:17:49 -0500

    button exercise
    added btn classname
    added children prop to display text within the button
    added onClick prop and function for in the event the button is clicked the log will display 'Clicked'

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import Alert from "./components/Alert";
+import Button from './components/Button';
 
 function App() {
-	
 	return (
-		<div className="alert alert-primary" role="alert">
-			<Alert> Hello <span>World</span> </Alert>
+		<div className='btn btn-primary'>
+			<Button onClick={() => console.log('Clicked')}>My Button</Button>
 		</div>
-	); 
+	);
 }
 
 export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,12 @@
+interface Props {
+    children: string;
+    onClick: () => void;
+}
+
+const Button = ({ children, onClick }: Props) => {
+  return (
+    <button className='btn btn-primary' onClick={onClick} >{children}</button>
+  )
+}
+
+export default Button
