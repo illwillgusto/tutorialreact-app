b46573df7f8e936188953e9070d7a23f0bf69829
Will Walton <will@endless3dge.com>
2024-03-25 16:44:10 -0500

    alert.tsx added
    added new component Alert.tsx
    
    removed the ListGroup.tsx code from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
-import ListGroup from './components/ListGroup';
+import Alert from "./components/Alert";
 
 function App() {
-	let items = ['Houston', 'New York', 'Chicago', 'Atlanta', 'Cleveland']; // this is moved from the ListGroups component because we added the interface function
-
-	const handleSelectItem = (item: string) => {
-		console.log(item);
-	};
+	
 	return (
 		<div>
-			<ListGroup
-				items={items}
-				heading='Cities'
-				onSelectItem={handleSelectItem}
-			/>{' '}
-			{/*this is in response to the interface function added so the item is 'items' and the heading is a string which is 'Cities' */}
+			<Alert></Alert>
 		</div>
-	); // worth noting each component has it's own state
+	); 
 }
 
 export default App;
