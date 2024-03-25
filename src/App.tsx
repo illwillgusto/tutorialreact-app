f0a96ea3a79f31c9f3e502d7d1ad764bcfc014ca
Will Walton <will@endless3dge.com>
2024-03-25 17:30:02 -0500

    optional props
    changed the button color
    added a "?" to the color prop, this tells the typescript complier that this prop is optional
    
    this was added after adding the primary color

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Button from './components/Button';
 function App() {
 	return (
 		<div className='btn btn-primary'>
-			<Button color="secondary" onClick={() => console.log('Clicked')}>My Button</Button>
+			<Button onClick={() => console.log('Clicked')}>My Button</Button>
 		</div>
 	);
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,10 @@
 interface Props {
     children: string;
-    color: string;
+    color?: string;
     onClick: () => void;
 }
 
-const Button = ({ children, onClick, color }: Props) => {
+const Button = ({ children, onClick, color = 'primary' }: Props) => {
   return (
     <button className={'btn btn-' + color} onClick={onClick} >{children}</button>
   )
