a70978a9d94217d6e6bb490315370c8013220fd9
Will Walton <will@endless3dge.com>
2024-03-25 17:22:33 -0500

    color prop
    added the color prop to the button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,12 @@
 interface Props {
     children: string;
+    color: string;
     onClick: () => void;
 }
 
-const Button = ({ children, onClick }: Props) => {
+const Button = ({ children, onClick, color }: Props) => {
   return (
-    <button className='btn btn-primary' onClick={onClick} >{children}</button>
+    <button className={'btn btn-' + color} onClick={onClick} >{children}</button>
   )
 }
 
